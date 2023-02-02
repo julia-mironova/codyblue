2ee589111af21d8ff73ac4bd872fe612d431fc2b
Yuliya Mironova <rabkevich15@gmail.com>
2023-02-02 11:31:45 +0100

    Init: react app with new favicon

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Check the product!
+        </p>        
+      </header>
+      <main className="App-main"></main>
+      <footer className="App-header">
+        <p>
+          Thank you! Don't forget about payment!
+        </p>
+        <a className="App-link" href="https://www.linkedin.com/in/mironava-julia-72a70845/">My linkedIn</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
