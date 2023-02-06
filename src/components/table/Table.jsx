d5160684fb05ed74139eb96e241874ec9ab493aa
Yuliya Mironova <rabkevich15@gmail.com>
2023-02-06 13:03:30 +0100

    Refactor colors

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -27,22 +27,6 @@ class TableComponent extends React.Component {
     });
   }
   
-  getColor = () => {   
-    return "green"
-  }
-
-  getColor2 = () => {   
-    return "yellow"
-  }
-
-   getColor3 = () => {   
-    return "pink"
-  }
-
-   getColor4 = () => {   
-    return "red"
-  }
-
   render() {
     const columns = [
     { field: 'id', headerName: 'ID', width: 170, headerClassName: 'header' },
@@ -51,17 +35,24 @@ class TableComponent extends React.Component {
    ];
 
 
-    //const colorsAll = [...this.props.rows.map(e => e.color)];
-    // const colors = Array.from(new Set(colorsAll))
-    // console.log(colors, "colors")
-    
+    const colorsAll = [...this.props.rows.map(e => e.color)];
+    //const colors = Array.from(new Set(colorsAll))
+       
     return (
       <Box className={styles.table} sx={{
         '& .header': { backgroundColor: "#32a1ce" },
-        '& .color': { backgroundColor: this.getColor },
-        '& .color2': { backgroundColor: this.getColor2 },
-        '& .color3': { backgroundColor: this.getColor3 },
-        '& .color4': { backgroundColor: this.getColor4 },
+        '& .color': { backgroundColor: colorsAll[0] },
+        '& .color1': { backgroundColor: colorsAll[1] },
+        '& .color2': { backgroundColor: colorsAll[2] },
+        '& .color3': { backgroundColor: colorsAll[3] },
+        '& .color4': { backgroundColor: colorsAll[4] },
+        '& .color5': { backgroundColor: colorsAll[5] },
+        '& .color6': { backgroundColor: colorsAll[6] },
+        '& .color7': { backgroundColor: colorsAll[7] },
+        '& .color8': { backgroundColor: colorsAll[8] },
+        '& .color9': { backgroundColor: colorsAll[9] },
+        '& .color10': { backgroundColor: colorsAll[10] },
+        '& .color11': { backgroundColor: colorsAll[11] }
         //https://mui.com/x/react-data-grid/style/
         //https://www.ag-grid.com/react-data-grid/row-styles/
         }}>
@@ -70,9 +61,23 @@ class TableComponent extends React.Component {
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
-        getRowClassName={(params) => {
-            //console.log(params.row.color, "params.row.color")
-            return params.row.color === "green" ? 'color' : (params.row.color === "yellow")?"color2": (params.row.color === "pink")?"color3":"color4"
+          getRowClassName={(params) => { 
+            const tookColor = params.row.color;
+            switch (tookColor) {
+              case colorsAll[0]: return "color";
+              case colorsAll[1]: return "color1";
+              case colorsAll[2]: return "color2";
+              case colorsAll[3]: return "color3";
+              case colorsAll[4]: return "color4";
+              case colorsAll[5]: return "color5";
+              case colorsAll[6]: return "color6";
+              case colorsAll[7]: return "color7";
+              case colorsAll[8]: return "color8";
+              case colorsAll[9]: return "color9";
+              case colorsAll[10]: return "color10";
+              case colorsAll[11]: return "color11";
+              default: return;
+            }
           }}        
         onRowClick={this.showInfo}
         />
--- src/fake_data.js
+++ src/fake_data.js
@@ -1,86 +1,85 @@
-export const products = [
-	{
+export const products = [{
     id: 1,
-    name: "toy",
+    name: "cerulean",
     year: 2000,
-    color: "green",
-    weight:5
+    color: "#98B2D1",
+    pantone_value: "15-4020"
   },
   {
     id: 2,
-    name: "cat",
-    year: 2100,
-    color: "red",
-    weight:10
+    name: "fuchsia rose",
+    year: 2001,
+    color: "#C74375",
+    pantone_value: "17-2031"
   },
   {
     id: 3,
-    name: "dog",
-    year: 2010,
-    color: "pink",
-    weight:1
+    name: "true red",
+    year: 2002,
+    color: "#BF1932",
+    pantone_value: "19-1664"
   },
   {
     id: 4,
-    name: "frog",
-    year: 2001,
-    color: "green",
-    weight:56
+    name: "aqua sky",
+    year: 2003,
+    color: "#7BC4C4",
+    pantone_value: "14-4811"
   },
   {
     id: 5,
-    name: "toy",
-    year: 2300,
-    color: "green",
-    weight:100
+    name: "tigerlily",
+    year: 2004,
+    color: "#E2583E",
+    pantone_value: "17-1456"
   },
   {
     id: 6,
-    name: "sss",
-    year: 2001,
-    color: "yellow",
-    weight:5
+    name: "blue turquoise",
+    year: 2005,
+    color: "#53B0AE",
+    pantone_value: "15-5217"
   },
   {
     id: 7,
-    name: "he",
-    year: 2030,
-    color: "blue",
-    weight:3
+    name: "sand dollar",
+    year: 2006,
+    color: "#DECDBE",
+    pantone_value: "13-1106"
   },
   {
     id: 8,
-    name: "toy",
-    year: 2000,
-    color: "green",
-    weight:5
+    name: "chili pepper",
+    year: 2007,
+    color: "#9B1B30",
+    pantone_value: "19-1557"
   },
   {
     id: 9,
-    name: "swan",
-    year: 3000,
-    color: "white",
-    weight:15
+    name: "blue iris",
+    year: 2008,
+    color: "#5A5B9F",
+    pantone_value: "18-3943"
   },
   {
     id: 10,
-    name: "toy",
-    year: 2000,
-    color: "grey",
-    weight:500
+    name: "mimosa",
+    year: 2009,
+    color: "#F0C05A",
+    pantone_value: "14-0848"
   },
   {
     id: 11,
-    name: "may",
-    year: 2500,
-    color: "no",
-    weight:5
+    name: "turquoise",
+    year: 2010,
+    color: "#45B5AA",
+    pantone_value: "15-5519"
   },
   {
     id: 12,
-    name: "boss",
-    year: 2000,
-    color: "green",
-    weight:500
+    name: "honeysuckle",
+    year: 2011,
+    color: "#D94F70",
+    pantone_value: "18-2120"
   }
 ]
